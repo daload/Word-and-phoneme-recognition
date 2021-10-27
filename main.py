2fc74390f30b5b7b406286b1f0bf3aa7705445a6
daload <david.l.adell11@gmail.com>
2021-10-27 13:18:13 +0200

    Added '__main__'

--- main.py
+++ main.py
@@ -2,42 +2,44 @@ from os import path
 from pocketsphinx.pocketsphinx import *
 import pronouncing
 
-MODELDIR = "./model"
-DATADIR = "./test/data"
+if __name__ == '__main__':
 
-# Create a decoder with certain model
-config = Decoder.default_config()
-config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
-config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
-config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
-config.set_string('-logfn','nul')
-decoder = Decoder(config)
+    MODELDIR = "./model"
+    DATADIR = "./test/data"
 
-# Decode streaming data.
-decoder = Decoder(config)
-decoder.start_utt()
+    # Create a decoder with certain model
+    config = Decoder.default_config()
+    config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+    config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+    config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+    config.set_string('-logfn','nul')
+    decoder = Decoder(config)
 
-# File names: 'f2bjrop1.0.wav', 'f2bjrop1.1.wav', 'f2btrop6.0.wav'
-stream = open(path.join(DATADIR, 'f2btrop6.0.wav'), 'rb')
+    # Decode streaming data.
+    decoder = Decoder(config)
+    decoder.start_utt()
 
-while True:
-    buf = stream.read(1024)
-    if buf:
-        decoder.process_raw(buf, False, False)
-    else:
-        break
-decoder.end_utt()
+    # File names: 'f2bjrop1.0.wav', 'f2bjrop1.1.wav', 'f2btrop6.0.wav'
+    stream = open(path.join(DATADIR, 'f2btrop6.0.wav'), 'rb')
+
+    while True:
+        buf = stream.read(1024)
+        if buf:
+            decoder.process_raw(buf, False, False)
+        else:
+            break
+    decoder.end_utt()
 
 
-# print('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])
-siln = ['</s>', '<s>', '<sil>']
-words = []
-for word in [seg.word for seg in decoder.seg()]:
-    if word not in siln:
-        new_word = ''
-        for char in word:
-            if char.isalpha() or char == "'":
-                new_word += char
-        print('{}: {}'.format(new_word, pronouncing.phones_for_word(new_word)))
+    # print('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])
+    siln = ['</s>', '<s>', '<sil>']
+    words = []
+    for word in [seg.word for seg in decoder.seg()]:
+        if word not in siln:
+            new_word = ''
+            for char in word:
+                if char.isalpha() or char == "'":
+                    new_word += char
+            print('{}: {}'.format(new_word, pronouncing.phones_for_word(new_word)))
 
 
